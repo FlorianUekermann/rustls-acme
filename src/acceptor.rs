6510d4c179898cf316efaeea68308d1f16a4c68c
Florian Uekermann <florian@uekermann.me>
2021-10-20 18:26:24 +0200

    switch to accept future

--- src/acceptor.rs
+++ src/acceptor.rs
@@ -1,9 +1,11 @@
 use crate::acme::ACME_TLS_ALPN_NAME;
 use crate::ResolvesServerCertUsingAcme;
 use async_rustls::rustls::{ServerConfig, Session};
-use async_rustls::server::TlsStream;
 use futures::{AsyncRead, AsyncWrite};
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
 #[derive(Clone)]
 pub struct TlsAcceptor {
@@ -17,17 +19,32 @@ impl TlsAcceptor {
         let config = Arc::new(config);
         TlsAcceptor { config }
     }
-    pub async fn accept<IO>(&self, stream: IO) -> std::io::Result<Option<TlsStream<IO>>>
+    pub fn accept<IO>(&self, stream: IO) -> Accept<IO>
     where
         IO: AsyncRead + AsyncWrite + Unpin,
     {
-        let tls = async_rustls::TlsAcceptor::from(self.config.clone())
-            .accept(stream)
-            .await?;
+        Accept(async_rustls::TlsAcceptor::from(self.config.clone()).accept(stream))
+    }
+}
+
+pub struct Accept<IO>(async_rustls::Accept<IO>);
+
+impl<IO: AsyncRead + AsyncWrite + Unpin> Future for Accept<IO> {
+    type Output = std::io::Result<Option<async_rustls::server::TlsStream<IO>>>;
+
+    #[inline]
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let tls = match Pin::new(&mut self.0).poll(cx) {
+            Poll::Ready(result) => match result {
+                Ok(tls) => tls,
+                Err(err) => return Poll::Ready(Err(err)),
+            },
+            Poll::Pending => return Poll::Pending,
+        };
         if tls.get_ref().1.get_alpn_protocol() == Some(ACME_TLS_ALPN_NAME) {
             log::debug!("completed acme-tls/1 handshake");
-            return Ok(None);
+            return Poll::Ready(Ok(None));
         }
-        Ok(Some(tls))
+        Poll::Ready(Ok(Some(tls)))
     }
 }
