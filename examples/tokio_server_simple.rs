4e9c25425e08506a2f61b658516644343569c481
Florian Uekermann <florian@uekermann.me>
2022-07-05 15:15:22 +0200

    add simple tokio example

---
+++ examples/tokio_server_simple.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+use futures::AsyncWriteExt;
+use futures::StreamExt;
+use futures::TryStreamExt;
+use rustls_acme::caches::DirCache;
+use rustls_acme::AcmeConfig;
+use std::path::PathBuf;
+use tokio_stream::wrappers::TcpListenerStream;
+use tokio_util::compat::TokioAsyncReadCompatExt;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Domains
+    #[clap(short, required = true)]
+    domains: Vec<String>,
+
+    /// Contact info
+    #[clap(short)]
+    email: Vec<String>,
+
+    /// Cache directory
+    #[clap(short, parse(from_os_str))]
+    cache: Option<PathBuf>,
+
+    /// Use Let's Encrypt production environment
+    /// (see https://letsencrypt.org/docs/staging-environment/)
+    #[clap(long)]
+    prod: Option<bool>,
+
+    #[clap(short, long, default_value = "443")]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    simple_logger::init_with_level(log::Level::Info).unwrap();
+    let args = Args::parse();
+
+    let tcp_listener = tokio::net::TcpListener::bind(format!("[::]:{}", args.port))
+        .await
+        .unwrap();
+    let tcp_incoming =
+        TcpListenerStream::new(tcp_listener).map_ok(|tcp_stream| tcp_stream.compat());
+
+    let mut tls_incoming = AcmeConfig::new(args.domains)
+        .contact(args.email.iter().map(|e| format!("mailto:{}", e)))
+        .cache_option(args.cache.clone().map(DirCache::new))
+        .incoming(tcp_incoming);
+
+    while let Some(tls) = tls_incoming.next().await {
+        let mut tls = tls.unwrap();
+        tokio::spawn(async move {
+            tls.write_all(HELLO).await.unwrap();
+            tls.close().await.unwrap();
+        });
+    }
+    unreachable!()
+}
+
+const HELLO: &'static [u8] = br#"HTTP/1.1 200 OK
+Content-Length: 10
+Content-Type: text/plain; charset=utf-8
+
+Hello Tls!"#;
